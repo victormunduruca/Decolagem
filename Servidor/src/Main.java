bc9bc12499e77cc7b16b31f70dc395254dd1ca31
victormunduruca <victormunduruca@gmail.com>
2017-08-20 19:29:41 -0300

    Primeira versão relógio Lamport

--- Servidor/src/Main.java
+++ Servidor/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.Scanner;
 
@@ -13,20 +14,41 @@ public class Main {
 	/**
 	 * @param args
 	 * @throws IOException 
+	 * @throws NotBoundException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, NotBoundException {
+		
 		System.out.println("Insira a porta");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int porta = Integer.parseInt(br.readLine());
-		System.out.println("Insira o nome do serviço");
-		String nome = br.readLine();
-		
-		Servidor servidor;
+		System.out.println("Insira o id");
+		int id = Integer.parseInt(br.readLine());;
+		Servidor servidor = null;
 		try {
-			servidor = new Servidor();
-			servidor.iniciar(porta, nome);
+			servidor = new Servidor(id);
+			servidor.iniciar(porta, id);
 		} catch (RemoteException e) {
 			e.printStackTrace();
+		}
+		
+		while(true) {
+			System.out.println("aperte um local, dois envia, 3 ");
+			switch (Integer.parseInt(br.readLine())) {
+			case 1:
+				servidor.getRelogioLamport().eventoLocal();
+				//servidor.testeServidores(1+"");
+				break;
+			case 2:
+				servidor.getRelogioLamport().eventoLocal();
+				System.out.println("Lamport antes de enviar a msg" +servidor.getRelogioLamport().getRelogio());
+				servidor.testeServidoresLamport();
+				break;
+			case 3:
+				System.out.println("RelogioLamport = " +servidor.getRelogioLamport().getRelogio());
+				break;
+			default:
+				break;
+			}
 		}
 	}
 
--- Servidor/src/network/Servidor.java
+++ Servidor/src/network/Servidor.java
@@ -1,7 +1,9 @@
 package network;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
@@ -13,24 +15,31 @@ import model.Trecho;
 
 public class Servidor extends UnicastRemoteObject implements IRemoto {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private ControllerServidor controller;
-
-	public Servidor() throws RemoteException {
+	private RelogioLamport relogio;
+	public Servidor(int id) throws RemoteException {
 		super();
-		controller = new ControllerServidor();
+		relogio = new RelogioLamport(id);
+		controller = new ControllerServidor(id);
 		try {
 			controller.configuraServidor();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
+	@Override
 	public String teste(String str) throws RemoteException {
+		relogio.eventoLocal();
         System.err.println("Requisicao: " + str);
         return "Ola";
 	}
 	
-	public boolean iniciar(int porta, String id) {
+	public boolean iniciar(int porta, int id) {
 	    try {
 	
 //	    	System.setProperty("java.rmi.server.hostname", "127.0.0.1");
@@ -49,10 +58,39 @@ public class Servidor extends UnicastRemoteObject implements IRemoto {
 	    }
 	    return true;
 	}
-
-	@Override
+	public void testeServidores(String id) throws MalformedURLException, RemoteException, NotBoundException {
+		IRemoto lookUp = null;
+		 for(int i = 1; i <= 2; i++) {
+			if(i != controller.getId()) {
+				 lookUp = (IRemoto) Naming.lookup("192.168.1.8/Decolagem"+i);
+				 lookUp.teste("EITAMOLEQUE");
+			}
+		 }
+		
+	}
+	public void testeServidoresLamport() throws MalformedURLException, RemoteException, NotBoundException {
+		IRemoto lookUp = null;
+		 for(int i = 1; i <= 2; i++) {
+			if(i != controller.getId()) {
+				 lookUp = (IRemoto) Naming.lookup("192.168.1.8/Decolagem"+i);
+				 lookUp.testeLamport(relogio.getRelogio());
+			}
+		 }
+		
+	}
+	
+ 	@Override
 	public Companhia getCompanhia() throws RemoteException {
 		return controller.getCompanhia();
+	}
+
+	@Override
+	public void testeLamport(float relogioMsg) throws RemoteException {
+		relogio.eventoMsg(relogioMsg);
+	}
+
+	public RelogioLamport getRelogioLamport() {
+		return relogio;
 	}
 
 }
